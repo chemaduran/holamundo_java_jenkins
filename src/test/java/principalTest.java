cf34eebd8d1e847b0df8c1e860f672b46b2181ce
Chema Durán <jgduran@gmail.com>
2020-09-20 12:43:24 +0200

    "probamos con tests"

---
+++ src/test/java/principalTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class principalTest {
+
+  @Test
+  public void testHelloWorld() {
+    PrintStream originalOut = System.out;
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bos));
+
+    // action
+    principal.main(null);
+
+    // assertion
+    assertEquals("Hola mundo!\n", bos.toString());
+
+    // undo the binding in System
+    System.setOut(originalOut);
+  }
+}
